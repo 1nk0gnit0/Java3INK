5e2ec6ec6bea2928415a3b090db05d9a939b6a04
1nk0gnit0 <inkognito_1987@list.ru>
2019-07-09 15:18:42 +0900

    Реализованы запись в историю и вывод последних 100 сообщений при авторизации

--- src/Lesson2/client/History.java
+++ src/Lesson2/client/History.java
@@ -1,47 +1,42 @@
 package Lesson2.client;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class History {
     private final static String FILE_NAME = "history.txt";
-    private final static int MAX_MESSAGES = 10;
+    private final static int MAX_MESSAGES = 100;
     private final static File file = new File(FILE_NAME);
 
     public static void addHistory(String message) throws FileNotFoundException {
-        int numbLine = 0;
         FileOutputStream fos = new FileOutputStream(file, true);
-        FileReader fr = new FileReader(file);
-        LineNumberReader lnr = new LineNumberReader(fr);
         byte [] buffer = message.getBytes();
 
         try {
-            if (file.exists()) {
-                if (numbLine <= MAX_MESSAGES){
-                    fos.write(buffer);
-                    numbLine++;
-                }else {
-                    fos.write(buffer);
-
-                }
-            }
+            if (file.exists()) fos.write(buffer);
             fos.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static String loadHistory () throws FileNotFoundException {
+    public static String loadHistory () throws IOException {
+        ArrayList<String> bandWidth = new ArrayList<String>();
         FileInputStream fis = new FileInputStream(file);
-        int i;
-        if(file.exists()) {
-            try {
-                while (true) {
-                    if (!((i = fis.read()) != -1)) return String.valueOf(((char) i));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+        String tmp;
+        while ((tmp = br.readLine()) != null)
+        {
+            bandWidth.add(tmp);
+            if (bandWidth.size() == MAX_MESSAGES + 1)
+                bandWidth.remove(0);
         }
-        return "";
+        String res = "";
+        fis.close();
+        for (String line : bandWidth){
+            res = line + "\n";
+        }
+        return res;
     }
 }
