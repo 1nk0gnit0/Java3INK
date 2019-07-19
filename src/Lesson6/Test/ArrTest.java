d44977b6aade4d9242053c8cb2334a0371e8395f
1nk0gnit0 <inkognito_1987@list.ru>
2019-07-19 15:47:01 +0900

    Реализовал параметризованный тест

--- src/Lesson6/Test/ArrTest.java
+++ src/Lesson6/Test/ArrTest.java
@@ -2,25 +2,50 @@ package Lesson6.Test;
 
 import Lesson6.Task;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
+import java.util.Collection;
 
-public class Test {
+@RunWith(Parameterized.class)
+public class ArrTest {
 
-    @org.junit.Test
-    public void checkOneOrFourTest(){
-        int [] arr = Task.creatArr();
-        System.out.println(Arrays.toString(arr));
-        Assert.assertTrue("False", Task.checkOneOrFour(arr));
+
+    @Parameterized.Parameters
+    public static Collection <Object[]> data(){
+        return Arrays.asList(new Object[][]{
+                {new int[]{1, 1, 4, 1, 1}, new int[]{1, 1}, true},
+                {new int[]{0, 0, 4, 0, 0, 0, 0}, new int[]{0, 0, 0, 0}, true},
+                {new int[]{2, 2, 2, 2, 2, 2, 2, 2},new int[]{}, false},
+                {new int[]{4, 4, 4, 1}, new int[]{1}, true}
+        });
+    }
+    private int[] addArr;
+    private int[] outArr;
+    private boolean expectedResult;
+
+    public ArrTest(int[] arr, int[] outArr, boolean expectedResult) {
+        this.addArr = arr;
+        this.outArr = outArr;
+        this.expectedResult = expectedResult;
     }
 
-    @org.junit.Test (expected = RuntimeException.class)
-    public void toArrayAfterFour(){
-        int [] arr = Task.creatArr();
-        System.out.println(Arrays.toString(arr));
-        int[] arr2 = Task.toArrayAfterFour(arr);
-        System.out.println(Arrays.toString(arr2));
-        Task.toArrayAfterFour(arr);
+    @Test
+    public void checkOneOrFourTest(){
+        Assert.assertEquals(expectedResult, Task.checkOneOrFour(addArr));
+    }
+
+    @Test
+    public void toArrayAfterFourTest(){
+        Assert.assertArrayEquals(outArr,Task.toArrayAfterFour(addArr));
+
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void toArrayAfterFourTestException(){
+        Task.toArrayAfterFour(addArr);
     }
 
 }
