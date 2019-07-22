5e589db461bf1bed482275eb310a3fee55d0ad60
1nk0gnit0 <inkognito_1987@list.ru>
2019-07-22 14:36:23 +0900

    Реализовано дз7

--- src/Lesson7/Main.java
+++ src/Lesson7/Main.java
@@ -1,8 +1,41 @@
 package Lesson7;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Main {
-    public static void main(String[] args) {
-        Class class = TestCase;
+    public static void main(String[] args) throws Exception {
+        Class aClass = MyClass.class;
+        Object obj = aClass.newInstance();
+        Method[] methods = aClass.getDeclaredMethods();
+        ArrayList<Method> al = new ArrayList<>();
+
+        Method before = null;
+        Method after = null;
+        for (Method o : aClass.getDeclaredMethods()) {
+            if (o.isAnnotationPresent(Test.class)) {
+                al.add(o);
+            }
+            if(o.isAnnotationPresent(BeforeSuite.class)) {
+                if(before == null) before = o;
+                else throw new RuntimeException("Больше одного метода с аннотацией BeforeSuite");
+            }
+            if(o.isAnnotationPresent(AfterSuite.class)) {
+                if(after == null) after = o;
+                else throw new RuntimeException("Больше одного метода с аннотацией AfterSuite");
+            }
+            al.sort(new Comparator<Method>() {
+                @Override
+                public int compare(Method o1, Method o2) {
+                    return o2.getAnnotation(Test.class).priority() - o1.getAnnotation(Test.class).priority();
+                }
+            });
+        }
+
+        if (before != null) before.invoke(obj,null);
+        for (Method o : al) o.invoke(obj,null);
+        if (after != null) after.invoke(obj,null);
     }
 
 
--- src/Lesson7/MyClass.java
+++ src/Lesson7/MyClass.java
@@ -1,22 +1,22 @@
 package Lesson7;
 
-public class TestCase {
+public class MyClass {
     @BeforeSuite
     public void init() {
         System.out.println("init");
     }
 
-    @Test(priority = 3)
+    @Test(priority = 1)
     public void test1() {
         System.out.println("test1");
     }
 
-    @Test(priority = 7)
+    @Test(priority = 2)
     public void test2() {
         System.out.println("test2");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 3)
     public void test3() {
         System.out.println("test3");
     }
